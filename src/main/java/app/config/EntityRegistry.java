4d271988177859303d61d57ffdcb0f5d477c66d6
EmilXavierVT <emilxavierthorsen@gmail.com>
2026-02-13 13:02:14 +0100

    initial setup of project config files for javalin and hibernate config files

---
+++ src/main/java/app/config/EntityRegistry.java
@@ -0,0 +1,14 @@
+package app.config;
+
+import app.entities.User;
+import org.hibernate.cfg.Configuration;
+
+final class EntityRegistry {
+
+    private EntityRegistry() {}
+
+    static void registerEntities(Configuration configuration) {
+        configuration.addAnnotatedClass(User.class);
+        // TODO: Add more entities here...
+    }
+}
